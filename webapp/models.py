1ea00212f373f604faa80b204d4f767fba489a4f
Sean McBride <2852861m@student.gla.ac.uk>
2025-03-10 12:18:49 +0000

    second commit

--- webapp/models.py
+++ webapp/models.py
@@ -1,34 +1,5 @@
-"""
 from django.db import models
 from django.contrib.auth.models import User
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-# Copied from tangowithdjango, think username and password stored in form.py in rango
-class UserProfile(models.Model):
-    user = models.ManyToManyField(User, on_delete=models.CASCADE)
-
-    #website = models.URLField(blank=True)
-    #picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    def __str__(self):
-        return self.user.username
-    
-class Event(models.Model):
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    organiser_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    date = models.DateTimeField(auto_now=False, auto_now_add=False)
-    location = models.CharField(max_length=100)
-    url = models.URLField()
-
-    def __str__(self):
-        return self.name
-"""
 
 
