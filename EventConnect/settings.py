a822ac73fefbee8ff2c55106196bb2e93c00b4d1
“Daniel <danielmmckean@gmail.com>
2025-03-28 09:43:34 +0000

    Trying to fiddle with things

--- EventConnect/settings.py
+++ EventConnect/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'wj58am*l=ns*s6%szw4btj38i5t^(^17wbp8b(7kxoe$k@844i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
 
 # Application definition
